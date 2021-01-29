7418d8280302f62f1e2be3d16ecb91220949c458
Vincent Bezault <vincent.bezault@icloud.com>
2021-01-29 16:20:32 +0100

    add buttons & display dice

--- script.js
+++ script.js
@@ -4,8 +4,23 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Start game
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Roll dice
+btnRoll.addEventListener('click', function () {
+  // 1 Random dice roll
+  const dice = Math.trunc(Math.random() * 6);
+
+  // 2 Display dice picture
+  diceEl.classList.remove('hidden');
+  diceEl.src = `Images/dice-${dice}.png`;
+
+  // 3 If roll 1 : change player
+});
