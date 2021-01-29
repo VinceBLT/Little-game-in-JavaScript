fae048d6186fa284902f963246f8ca1ad6d3c126
Vincent Bezault <vincent.bezault@icloud.com>
2021-01-29 22:39:37 +0100

    init function and last improvements

--- script.js
+++ script.js
@@ -16,15 +16,25 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Start game
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Start game
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  diceEl.classList.add('hidden');
+};
+init();
 
 const switchPlayer = function () {
   document.querySelector(`#current--${activePlayer}`).textContent = 0;
@@ -88,11 +98,5 @@ btnHold.addEventListener('click', function () {
 
 // New game
 btnNew.addEventListener('click', function () {
-  playing = true;
-  scores = [0, 0];
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  diceEl.classList.add('hidden');
+  init();
 });
