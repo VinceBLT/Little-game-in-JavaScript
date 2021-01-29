066ff13a1811a259f1454c461afa7c76c7896942
Vincent Bezault <vincent.bezault@icloud.com>
2021-01-29 17:03:33 +0100

    switch style with players

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 'use strict';
 
 // Select elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const dot0El = document.querySelector('.dot--0');
+const dot1El = document.querySelector('.dot--1');
+const name0El = document.querySelector('#name--0');
+const name1El = document.querySelector('#name--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -39,5 +45,11 @@ btnRoll.addEventListener('click', function () {
     document.querySelector(`#current--${activePlayer}`).textContent = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    dot0El.classList.toggle('hidden');
+    dot1El.classList.toggle('hidden');
+    name0El.classList.toggle('name--inactive');
+    name1El.classList.toggle('name--inactive');
   }
 });
