167ba29ab632d2bcfddb64f05128fd828a9cddcf
Vincent Bezault <vincent.bezault@icloud.com>
2021-01-29 19:22:05 +0100

    hold score + switch player function

--- script.js
+++ script.js
@@ -21,8 +21,21 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  dot0El.classList.toggle('hidden');
+  dot1El.classList.toggle('hidden');
+  name0El.classList.toggle('name--inactive');
+  name1El.classList.toggle('name--inactive');
+};
 
 // Roll dice
 btnRoll.addEventListener('click', function () {
@@ -41,15 +54,19 @@ btnRoll.addEventListener('click', function () {
       `#current--${activePlayer}`
     ).textContent = currentScore;
   } else {
-    // Switch user
-    document.querySelector(`#current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-    dot0El.classList.toggle('hidden');
-    dot1El.classList.toggle('hidden');
-    name0El.classList.toggle('name--inactive');
-    name1El.classList.toggle('name--inactive');
+    // Switch player
+    switchPlayer();
   }
+});
+
+// Hold score
+btnHold.addEventListener('click', function () {
+  // Add current to player's score
+  scores[activePlayer] += currentScore;
+  document.querySelector(`current--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // Check if score is >= 100
+
+  // Switch next player
+  switchPlayer();
 });
